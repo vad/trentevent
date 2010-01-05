9fb1f5115db5b65a60e9f97755147c918e036a90
Davide Setti <davide.setti@gmail.com>
2010-01-05 09:30:53 +0100

    add supercinemarovereto.it test data

---
+++ test/data/rassegne.php
@@ -0,0 +1,123 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML><HEAD><TITLE>SUPERCINEMAROVERETO</TITLE>
+<META http-equiv=content-type content=text/html;charset=iso-8859-1>
+<META content="" name=DESCRIPTION>
+<META content="" name=keywords>
+<LINK href="images/stile.css" 
+rel=stylesheet>
+<SCRIPT language=JavaScript>
+<!-- Begin
+function popUp(URL) {
+day = new Date();
+id = day.getTime();
+eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=0,width=480,height=500,left=200,top=20');");
+}
+// End -->
+</SCRIPT>
+
+<META content="MSHTML 6.00.2600.0" name=GENERATOR>
+<link rel="stylesheet" href="images/rosmini.css" type="text/css">
+</HEAD>
+<BODY text=#cccccc vLink=#a9a9a9 aLink=black link=#cccc99 bgColor=black>
+<table width="740" border="0" cellspacing="0" cellpadding="0" align="center">
+  <tr> 
+    <td><a href="index.php"><img src="images/logo2.gif" width="350" height="65" border="0"></a></td>
+  </tr>
+  <tr> 
+    <td height="6"> 
+      <div align="right" class="main"> </div>
+    </td>
+  </tr>
+</table>
+<table width="740" border="0" cellspacing="0" cellpadding="0" align="center">
+  <tr> 
+    <td height="26" valign="middle"> 
+      <div align="right"> 
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr> 
+            <td width="38%" height="26"> 
+              <div align="left" class="footer2"> MULTISALA <img src="images/spazio.gif" width="8" height="8"> 
+                ROVERETO <img src="images/spazio.gif" width="8" height="8"> (TN)</div>
+            </td>
+            <td width="62%" height="26"> 
+              <div align="right"><a href="index.php" class="menu_link">HOME</a> 
+                <img src="images/spazio.gif" width="15" height="8"><a href="dove_siamo.php" class="menu_link">DOVE 
+                SIAMO</a> <img src="images/spazio.gif" width="15" height="8"><a href="servizi.php" class="menu_link">SERVIZI 
+                - NEWSLETTER</a> <img src="images/spazio.gif" width="15" height="8"><a href="sale.php" class="menu_link">SALE</a> 
+                <img src="images/spazio.gif" width="15" height="8"><span class="menu_link2">RASSEGNE</span> 
+                <img src="images/spazio.gif" width="15" height="8"><a href="mailto:info@supercinemarovereto.it" class="menu_link">E-MAIL</a></div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </td>
+  </tr>
+</table>
+<TABLE cellSpacing=0 cellPadding=0 width=740 bgColor=#333333 border=0 height="347" align="center">
+  <TBODY> 
+  <TR height=320> 
+    <TD vAlign=top width=30>&nbsp;</TD>
+    <TD vAlign=top width=710> 
+      <table width="530" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td height="6"><img src="images/spazio.gif" width="10" height="8"></td>
+        </tr>
+      </table>
+	   <!-- -->
+	    <table width="670" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td class="main"><p>&nbsp;rassegna ottobre - dicembre 2009</p>
+<p>01/10 VIDEOCRACY - BASTA APPARIRE di Erik Gandini (Svezia 2009)</p>
+<p>08/10 CATTIVO TENENTE - ULTIMA CHIAMATA NEW ORLEANS di Werner Herzog (USA - 209/</p>
+<p>15/10 RACCONTI ETA' DELL'ORO di Cristian Mongiu (Romania 2009)</p>
+<p>22/10 CHERI di Stephen Frears (Gran Bretagna 2009)</p>
+<p>29/10 AMORI E DISASTRI di Sarah Watt (Australia 2009)</p>
+<p>&nbsp;</p>
+<p>05/11 MOTEL WOODSTOCK di Ang Lee (USA - 2009)</p>
+<p>12/11 COSMONAUTA di Susanna NIcchiarelli (Italia 2008)</p>
+<p>19/11 LO SPAZIO BIANCO di Francesca Comencini (Italia 2009)</p>
+<p>26/11 CAPITALISM : A LOVE STORY di Michael Moore (USA 2009)</p>
+<p>&nbsp;</p>
+<p>03/12 LEBANON di Samuel Maoz (Israele 2009)</p>
+<p>10/12 IL NASTRO BIANCO di Michael Haneke (Austria 2009)</p>
+<p>17/12 SEGRETI DI FAMIGLIA di F. Ford Coppola (USA 2009)</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p></td>
+        </tr>
+        <tr> 
+          <td height="3"><img src="images/spazio.gif" width="10" height="10"></td>
+        </tr>
+      </table>
+	   <!-- -->
+	  	  <!-- -->
+      <table width="670" border="0" cellspacing="0" cellpadding="0" height="8">
+        <tr>
+          <td class="main-vvsml"><img src="images/spazio.gif" width="6" height="6"></td>
+        </tr>
+        <tr> 
+          <td class="main-vvsml" height="2"> 
+            <div align="right"></div>
+          </td>
+        </tr>
+        <tr> 
+          <td class="main-sml" height="2"><img src="images/spazio.gif" width="6" height="6"></td>
+        </tr>
+      </table>
+    </TD>
+  </TR>
+  </TBODY> 
+</TABLE>
+<table cellspacing=0 cellpadding=4 width=740 border=0 height="26" align="center">
+  <tbody> 
+  <tr height=30> 
+    <td height=26 width="660"><span class=footer>© 2009 SUPERCINEMAROVERETO - 
+      Piazza Rosmini 18/A. Rovereto (TN) - tel. 0464 421216 - P.Iva 01192330221 
+      | </span><a href="privacy.php" class="footer">Privacy</a></td>
+    <td height=26 width="80"> 
+      <div align=right> <a href="http://www.ensox.com" target="_blank" class="footer">EnsoX.com</a></div>
+    </td>
+  </tr>
+  </tbody> 
+</table>
+<p>&nbsp;</p>
+</BODY></HTML>
